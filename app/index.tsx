1c0fddd8b0727d167c6f27e51e4b45164b5b1597
Farel <farelwisnu12345@gmail.com>
2026-09-08 06:39:34 +0700

    menambah cihuy pada index

--- app/index.tsx
+++ app/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>cihuy</Text>
       {/* <VariabelSoal /> */}
       {/* <KondisiSoal /> */}
       {/* <LoopingSoal /> */}
